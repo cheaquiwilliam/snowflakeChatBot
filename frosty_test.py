94443a2c03d53335859a446371df185f0e2d88d3
cheaquiwilliam <158086886+cheaquiwilliam@users.noreply.github.com>
2024-01-30 11:50:50 -0500

    Create frosty_test.py
    intial

---
+++ frosty_test.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+import re
+
+from utils import *
+
+
+st.title("Echo Chatbot")
+prompt = st.chat_input()
+
+if prompt:
+  st.chat_message("user").write(prompt)
+  st.chat_message("assistant").write(prompt)
