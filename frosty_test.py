3dbb9b0fab423270698daae7bc9c0c1f6e3bb234
cheaquiwilliam <158086886+cheaquiwilliam@users.noreply.github.com>
2024-01-30 11:56:05 -0500

    Update frosty_test.py

--- frosty_test.py
+++ frosty_test.py
@@ -9,5 +9,5 @@ st.title("Echo Chatbot")
 prompt = st.chat_input()
 
 if prompt:
-  st.chat_message("user", avatar = Avatars.USER).write(prompt)
+  st.chat_message("user").write(prompt)
   st.chat_message("assistant").write(prompt)
