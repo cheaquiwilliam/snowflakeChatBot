6dcc793f3e4e14f8cc2535cb1eaf70cf83f48533
cheaquiwilliam <158086886+cheaquiwilliam@users.noreply.github.com>
2024-01-30 12:05:40 -0500

    Update frosty_test.py

--- frosty_test.py
+++ frosty_test.py
@@ -5,7 +5,7 @@ import re
 # from utils import *
 
 
-st.title("Echo Chatbot")
+st.title("Conversational Chatbot")
 
 if "messages" not in st.session_state:
   st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
@@ -15,7 +15,8 @@ prompt = st.chat_input()
 
 if prompt:
   st.session_state.messages.append({"role": "user", "content": prompt})
-  st.session_state.messages.append({"role": "assistant", "content": prompt})
+  response = call_llm_open_ai(st.session_state.messages)
+  st.session_state.messages.append({"role": "assistant", "content": response})
 for msg in st.session_state.messages:
   if msg.get("role") == "user":
     st.chat_message("user").write(msg.get("content"))
