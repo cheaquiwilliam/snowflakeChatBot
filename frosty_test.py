e1dd496ddbee04c14e8e72cdcd1c1779a430362e
cheaquiwilliam <158086886+cheaquiwilliam@users.noreply.github.com>
2024-01-30 12:02:08 -0500

    Update frosty_test.py

--- frosty_test.py
+++ frosty_test.py
@@ -6,8 +6,18 @@ import re
 
 
 st.title("Echo Chatbot")
+
+if "messages" not in st.session_state:
+  st.session_state.messages = [{"role"; "assistant", "content": "How can I help you?"}]
+  
+
 prompt = st.chat_input()
 
 if prompt:
-  st.chat_message("user").write(prompt)
-  st.chat_message("assistant").write(prompt)
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  st.session_state.messages.append({"role": "assistant", "content": prompt})
+for msg in st.session_state.messages:
+  if msg.get("role") == "user":
+    st.chat_message("user").write(msg.get("content"))
+  elif msg.get("role" == "assistant":
+    st.chat_message("assistant").write(msg.get("content"))
