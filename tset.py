44e52297b4e7cf7d9adfdcd1963513bf7a3bbd1e
cheaquiwilliam <158086886+cheaquiwilliam@users.noreply.github.com>
2024-02-01 13:44:53 -0500

    Update tset.py

--- tset.py
+++ tset.py
@@ -48,7 +48,8 @@ for message in st.session_state.messages:
     if message["role"] == "system":
         continue
     with st.chat_message(message["role"]):
-        st.write(unmask_response_statement(message["content"], mapped))
+        # st.write(unmask_response_statement(message["content"], mapped))
+          st.write(message["content"])
         if "results" in message:
             st.dataframe(unmask_response_statement(message["results"], mapped))
         # print(unmask_response_statement(message["results"], mapped))
